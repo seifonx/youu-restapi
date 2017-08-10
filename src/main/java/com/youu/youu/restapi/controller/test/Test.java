4562cc07102e648c928c54a87e7249dd8e1609ca
Seifon <Seifon@163.com>
2017-08-10 10:18:30 +0800

    打通页面，解决404

--- src/main/java/com/youu/youu/restapi/controller/test/Test.java
+++ src/main/java/com/youu/youu/restapi/controller/test/Test.java
@@ -1,6 +1,16 @@
 package com.youu.youu.restapi.controller.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class Test {
+    
+    @ResponseBody
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello";
+    }
 
 }
